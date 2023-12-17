7f2ba4312f97cee9164abbdd076be7fff3207387
Caio Reis <fernandes.caio@estudante.ufjf.br>
2023-12-17 14:54:46 -0300

    jogo.js atualizado

--- src/jogo.js
+++ src/jogo.js
@@ -1,53 +1,53 @@
-const btnRolarDado = document.getElementById('RolarDados');
-const btnCol1 = document.getElementById('col1');
-const btnCol2 = document.getElementById('col2');
-const btnCol3 = document.getElementById('col3');
+const btnRolarDado = document.getElementById('RolarDados')
 
-let i = 1;
-let j = 4;
-let k = 7;
-let cont = 0;
+const btnCol1 = document.getElementById('col1')
+const btnCol2 = document.getElementById('col2')
+const btnCol3 = document.getElementById('col3')
 
+let i = 1
+let j = 4
+let k = 7
+let cont = 0
 
-btnRolarDado.addEventListener('click', mostraDado);
+// gerar número aleatório
+function randomNumber(){
+    return Math.floor(Math.random() * 6) + 1;
+}
+btnRolarDado.addEventListener('click', mostraDado)
 
-function mostraDado() {
-    let celula = document.getElementById("Jog1");
-    let numAleatorio = Math.floor(Math.random() * 6) + 1;
-    celula.innerHTML = numAleatorio;
-    return 
-    numAleatorio;
+// mostrar dado
+function mostraDado(){
+    let celula = document.getElementById("Jog1")
+    celula.innerHTML = randomNumber();
 }
 
-btnCol1.addEventListener('click', escolheColuna1);
+btnCol1.addEventListener('click', escolheColuna1)
+btnCol2.addEventListener('click', escolheColuna2)
+btnCol3.addEventListener('click', escolheColuna3)
 
 function escolheColuna1() {
-    if(i<=3){
-        let cel = document.getElementById("celula"+ i);
-        let num = mostraDado();
-        cel.innerHTML = num;
-        i++;
+
+    if( i <= 3 ){
+        let cel = document.getElementById("celula" + i)
+        cel.innerHTML = randomNumber();
+        i++
     }
 }
 
-btnCol2.addEventListener('click', escolheColuna2);
 
 function escolheColuna2() {
     if(j<=6){
         let cel = document.getElementById("celula"+ j);
-        let num = mostraDado();
-        cel.innerHTML = num;
+        cel.innerHTML = randomNumber()
         j++;
     }
 }
 
-btnCol3.addEventListener('click', escolheColuna3);
 
 function escolheColuna3(){
     if(k<=9){
         let cel = document.getElementById("celula"+ k);
-        let num = mostraDado();
-        cel.innerHTML = num;
+        cel.innerHTML = randomNumber()
         k++;
     }
 }
