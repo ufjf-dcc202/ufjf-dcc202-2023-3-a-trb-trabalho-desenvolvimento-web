5efc183e74db2aa9cb9a4e7a0b48056d74c6d8ef
Luiza Caldeira <luiza.caldeira@estudante.ufjf.br>
2023-12-17 12:17:49 -0300

    Iniciando a primeira da lógica do código em Java Script

--- src/jogo.js
+++ src/jogo.js
@@ -1,30 +1,53 @@
 const btnRolarDado = document.getElementById('RolarDados');
+const btnCol1 = document.getElementById('col1');
+const btnCol2 = document.getElementById('col2');
+const btnCol3 = document.getElementById('col3');
 
-//Ao clicar no botão a tabela é preenchida.
+let i = 1;
+let j = 4;
+let k = 7;
+let cont = 0;
 
-btnRolarDado.addEventListener('click', preencheTabela);
-var i = 1;
 
-function randomizarNumeros() {
-    //Randomiza números de 1 a 6.
-    return Math.floor(Math.random() * 6) + 1;
+btnRolarDado.addEventListener('click', mostraDado);
+
+function mostraDado() {
+    let celula = document.getElementById("Jog1");
+    let numAleatorio = Math.floor(Math.random() * 6) + 1;
+    celula.innerHTML = numAleatorio;
+    return 
+    numAleatorio;
 }
 
-function preencheCelula_Jog() { 
-    //A primeira célula de índice 1 será preenchida com o número aleatório.
-        var celula = document.getElementById("celula" + i);
-        var numeroAleatorio = randomizarNumeros();
-        celula.innerHTML = numeroAleatorio;
-}
-function preencheCelula_Opn() {
-        var celula = document.getElementById("celula" + (i+1));
-        var numeroAleatorio = randomizarNumeros();
-        celula.innerHTML = numeroAleatorio;  
+btnCol1.addEventListener('click', escolheColuna1);
+
+function escolheColuna1() {
+    if(i<=3){
+        let cel = document.getElementById("celula"+ i);
+        let num = mostraDado();
+        cel.innerHTML = num;
+        i++;
+    }
 }
 
-function preencheTabela(){
-    //Atualiza o índice para a próxima célula ser preenchida.
-    preencheCelula_Jog();
-    preencheCelula_Opn();
-    i+=2;
+btnCol2.addEventListener('click', escolheColuna2);
+
+function escolheColuna2() {
+    if(j<=6){
+        let cel = document.getElementById("celula"+ j);
+        let num = mostraDado();
+        cel.innerHTML = num;
+        j++;
+    }
+}
+
+btnCol3.addEventListener('click', escolheColuna3);
+
+function escolheColuna3(){
+    if(k<=9){
+        let cel = document.getElementById("celula"+ k);
+        let num = mostraDado();
+        cel.innerHTML = num;
+        k++;
+    }
 }
