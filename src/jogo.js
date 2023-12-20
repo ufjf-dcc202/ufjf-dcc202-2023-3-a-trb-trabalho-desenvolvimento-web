8c352288c4307214a22ab96d3a4cfe7da0423bbe
Luiza Caldeira <luiza.caldeira@estudante.ufjf.br>
2023-12-20 13:13:35 -0300

    Volta da lógica antiga, usando vetor para armazenar valor sorteado.

--- src/jogo.js
+++ src/jogo.js
@@ -1,88 +1,85 @@
 //jogo.js
-    
-let currentPlayer = 1;
-let player1Board = Array(9).fill(0);
-let player2Board = Array(9).fill(0);
+const btnRolarDado = document.getElementById('RolarDados');
+const btnCol1 = document.getElementById('col1');
+const btnCol2 = document.getElementById('col2');
+const btnCol3 = document.getElementById('col3');
 
-// Função para rolar o dado
-    function rollDice(player) {
-        const diceValue = Math.floor(Math.random() * 6) + 1;
+const btnReinicia = document.getElementById ('Reiniciar')
 
-        let currentPlayerBoard;
-        let opponentBoard;
-            
-        if (player===1){
-            currentPlayerBoard = player1Board;
-        }
-        else{
-            currentPlayerBoard = player2Board;
-        }
-            
-        if (player===1){
-            opponentBoard = player2Board;
-        }
-        else{
-            opponentBoard = player1Board;
-        }
-            
-        // Atualiza o tabuleiro com o valor do dado
-        const emptyCellIndex = currentPlayerBoard.indexOf(0); //Retorna o índice do primeiro valor na array (se não houver, retorna -1)
-        if (emptyCellIndex !== -1) {
-            currentPlayerBoard[emptyCellIndex] = diceValue;
+let i = 1;
+let j = 4;
+let k = 7;
+let cont = 1;
+let numAleatorio=[];
 
-            // descarta o dado do oponente com o mesmo valor
-            const opponentEmptyCellIndexes = [];
-            opponentBoard.forEach((value, index) => {
-                if (value === diceValue) {
-                    opponentBoard[index] = 0;
-                    opponentEmptyCellIndexes.push(index);
-                    }
-                });
-                    
-            updateBoard(player);
-            updateBoard(3 - player);  // Atualiza o tabuleiro do oponente
+//sorteia e joga o num sorteado na tela
 
-            // Checa se o jogo acabou
-            if (currentPlayerBoard.every(value => value !== 0)) {
-                alert(`Player ${player} wins!`);
-                resetGame();
-                }
+btnRolarDado.addEventListener('click', sorteiaDado);
+function sorteiaDado() {
+    let celula = document.getElementById ('Jog1');
+    numAleatorio = Math.floor(Math.random() * 6) + 1;
+    celula.innerHTML = numAleatorio;
+}
+// coloca esse mesmo numero dentro das celulas de cada coluna escolhida
 
-            // Troca players
-            currentPlayer = 3 - player;  // alternar entre jogador 1 e 2
-
-            // Se for a vez do oponente, simula uma jogada
-            if (currentPlayer === 2) {
-                setTimeout(() => rollDice(2), 1000);
-            }
-        }
+btnCol1.addEventListener('click', escolheColuna1);
+function escolheColuna1() {
+    if(i<=3){
+        let cel = document.getElementById("celula"+ i);
+        cel.innerHTML = numAleatorio;
+        i++;
     }
+    jogadaoponente();
+}
 
-// função para atualizar o tabuleiro
-function updateBoard(player) {
-    const boardElement = document.getElementById(`player${player}-board`);
-    boardElement.innerHTML = '';
-
-    const currentPlayerBoard = player === 1 ? player1Board : player2Board;
-
-    for (let i = 0; i < currentPlayerBoard.length; i++) {
-        const cell = document.createElement('div');
-        cell.className = 'cell';
-        cell.textContent = currentPlayerBoard[i] !== 0 ? currentPlayerBoard[i] : '';
-        boardElement.appendChild(cell);
+btnCol2.addEventListener('click', escolheColuna2);
+function escolheColuna2() {
+    if(j<=6){
+        let cel = document.getElementById("celula"+ j);
+        cel.innerHTML = numAleatorio;
+        j++;
     }
+    jogadaoponente();
+}
+btnCol3.addEventListener('click', escolheColuna3);
+function escolheColuna3(){
+    if(k<=9){
+        let cel = document.getElementById("celula"+ k); 
+        cel.innerHTML = numAleatorio;
+        k++;
+    }
+    jogadaoponente();
 }
 
-// função para resetar o jogo
-function resetGame() {
-    currentPlayer = 1;
-    player1Board = Array(9).fill(0);
-    player2Board = Array(9).fill(0);
-    updateBoard(1);
-    updateBoard(2);
+//jogadas do oponente
+function jogadaoponente (){
+        if (cont<=9){
+        let cel = document.getElementById("celula1."+ cont);
+        cel.innerHTML = Math.floor(Math.random() * 6) + 1;
+        cont++;
+        }
 }
 
-window.onload = function () {
-    updateBoard(1);
-    updateBoard(2);
+
+
+btnReinicia.addEventListener('click', reiniciarJogo);
+    function reiniciarJogo() {
+        // Limpar conteúdo das células
+        let celula = document.getElementById ('Jog1');
+        celula.innerHTML = '&nbsp';
+
+        for (let i = 1; i <= 9; i++) {
+          let celula = document.getElementById("celula" + i);
+          celula.innerHTML = '&nbsp';
+        }
+        for (let i = 1; i <= 9; i++) {
+            let celula = document.getElementById("celula1." + i);
+            celula.innerHTML = '&nbsp';
+        }
+        // Reiniciar variáveis
+        i = 1;
+        j = 4;
+        k = 7;
+        cont = 0;
 }
+
